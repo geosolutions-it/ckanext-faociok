af86492beb1a574938e5f2cb2d708efd5b2d016f
Cezary Statkiewicz <cezio@thelirium.net>
2018-05-14 13:05:26 +0200

    schema for m49 regions #15

--- ckanext/faociok/schema.py
+++ ckanext/faociok/schema.py
@@ -18,6 +18,13 @@ def _get_package_schema():
          'description': _("Select data type of dataset"),
          'is_required': True,
          },
+         {'name': 'fao_m49_region',
+          'validators': [t.get_validator('fao_m49_region')],
+          'element': 'select',
+          'label': _("M49 Region"),
+          'vocabulary_name': Vocabulary.VOCABULARY_M49_REGION,
+          'description': _("Regions according to UN M.49 Standard"),
+          'is_required': False},
     ]
 
 def get_create_package_schema():
