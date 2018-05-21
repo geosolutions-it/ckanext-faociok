565ba486e4ba167e030d0478575ba505bb746dff
Cezary Statkiewicz <cezio@thelirium.net>
2018-05-21 13:35:28 +0200

    localized/facet compatible link for locations #17

--- ckanext/faociok/helpers.py
+++ ckanext/faociok/helpers.py
@@ -4,6 +4,7 @@
 import json
 from ckan.lib.i18n import get_lang
 import ckan.logic as logic
+from ckan.lib import helpers as h
 from ckanext.faociok.models import Vocabulary, VocabularyTerm
 
 
@@ -41,3 +42,12 @@ def fao_get_action(action_name, data_dict=None):
     if data_dict is None:
         data_dict = {}
     return logic.get_action(action_name)({}, data_dict)
+
+def get_url_for_location(location_code):
+    lang = get_lang()
+    term = VocabularyTerm.get(Vocabulary.VOCABULARY_M49_REGIONS, location_code)
+    if not term:
+        return h.url_for('search')
+    label = term.get_label(lang).label or term.get_label('en').label
+    qdict = {'fao_m49_regions_l{}_{}'.format(term.depth, lang): label}
+    return h.url_for('search', **qdict)
