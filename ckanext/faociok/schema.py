3f0dba289ad0937e09dd5f468486026ea7bd2f4a
Cezary Statkiewicz <cezio@thelirium.net>
2018-05-22 15:17:33 +0200

    #22: remove L2 regions
    Fix geosolutions-it/C013-FAO-CIOK-CKAN#22
    
    * datatype field, load command
    
    * datatype field in ui
    
    * datatype field display #20
    
    * localized datatype in package read template
    
    * facets #20
    
    * updated i18n
    
    * updated i18n
    
    * m49 data importer
    
    * m49 importer
    
    * data files #15, #20
    
    * schema for m49 regions #15
    
    * improved m49 import, misc improvements #15
    
    * en labels for non-en languages #15
    
    * m49 - localization for other languages, form improvements #15
    
    * display m49 regions #15
    
    * index whole m49 path #49
    
    * index per-region type  #15
    
    * updated translations
    
    * home layout #17
    
    * handle single/empty values
    
    * use db arrays for multiple values for home view
    
    * process requirements within setup.py
    
    * streamlined fao featured orgs/groups api #14
    
    * localized/facet compatible link for locations #17
    
    * locations review fixes for #21
    
    * improved filtering of countries
    
    * proper counts of locations
    
    * refactored term parents count query
    
    * remove l2 from form
    
    * order of countries
    
    * restored correct location  url

--- ckanext/faociok/schema.py
+++ ckanext/faociok/schema.py
@@ -5,7 +5,7 @@
 from ckan.common import _, ungettext
 import ckan.plugins.toolkit as t
 from ckan.plugins import PluginImplementations
-from ckanext.faociok.models import Vocabulary
+from ckanext.faociok.models import Vocabulary, VocabularyTerm, VocabularyLabel
 
 
 def _get_package_schema():
@@ -17,8 +17,8 @@ def _get_package_schema():
          'vocabulary_name': Vocabulary.VOCABULARY_DATATYPE,
          'description': _("Select data type of dataset"),
          'multiple': False,
-         'type': 'raw',
          'additional_module': None,
+          'vocabulary_filters': {},
          'autocomplete': False,
          'is_required': True,
          },
@@ -26,11 +26,12 @@ def _get_package_schema():
           'validators': [t.get_validator('ignore_missing'), t.get_validator('fao_m49_regions')],
           'element': 'select',
           'multiple': True,
-          'type': 'json',
           'autocomplete': True,
           'label': _("M49 Regions"),
           'additional_module': 'm49_regions',
           'vocabulary_name': Vocabulary.VOCABULARY_M49_REGIONS,
+          'vocabulary_filters': [VocabularyTerm.depth == 1],
+          'vocabulary_order_by': [VocabularyLabel.label],
           'description': _("Regions according to UN M.49 Standard"),
           'is_required': False},
     ]
